fa877f3f4660e24924d6e35f392b033fae0fed04
Om Mahajna <omrmahajan9477@gmail.com>
2025-10-12 23:09:12 +0530

    refactor login functionality

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -70,6 +70,10 @@ export const login = async (req, res) => {
 
     const {email, password} = req.body 
 
+    if (!email || !password) {
+       return res.status(400).json({ message: "Email and password are required" });
+    }
+
     try {
         const user = await User.findOne({email})
         if(!user) return res.status(400).json({message:"Invalid credentials"}) ;
