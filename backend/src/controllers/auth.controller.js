a5f5949cecfc2d0da9c2b56a2e8c83183f9ce106
Om Mahajna <omrmahajan9477@gmail.com>
2025-10-18 20:22:06 +0530

    update ProfilePic functionality added with /check verification

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,8 +1,9 @@
-import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import { generateToken } from "../lib/utils.js";
 import { sendWelcomeEmail } from "../emails/emailHandlers.js";
 import { ENV } from "../lib/env.js";
+import cloudinary from "../lib/cloudinary.js";
+import User from "../models/User.js";
 
 export const signup = async (req, res) => {
     const { fullName, email, password } = req.body;
@@ -100,3 +101,23 @@ export const logout = (_, res) => {
     res.cookie("jwt", "" , {maxAge:0}) ;
     res.status(200).json({message:"Logged out successfully"});
 };
+
+export const updateProfile = async (req, res) => {
+    try {
+        const {profilePic} = req.body ;
+        if(!profilePic) return res.status(400).json({message:"Profile pic is required"});
+
+        const userId = req.user._id ;
+        const uploadResponse = await cloudinary.uploader.upload(profilePic)
+
+        const updatedUser = await User.findByIdAndUpdate(userId, {profilePic: uploadResponse.secure_url}, {new: true});
+
+        res.status(200).json(updatedUser);
+
+    } catch (error) {
+
+        console.log("Error in update profile:", error);
+        res.status(500).json({message:"Internal server error"}) ;    
+        
+    }
+} ;
