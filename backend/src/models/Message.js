a1d98b7a11e468504de8a7171e571ffbdf5aaf06
Om Mahajna <omrmahajan9477@gmail.com>
2025-10-19 12:58:13 +0530

    make some minor changes

--- backend/src/models/Message.js
+++ backend/src/models/Message.js
@@ -15,6 +15,8 @@ const messageSchema = new mongoose.Schema(
         },
         text: {
             type: String,
+            trim: true,
+            maxlength: 2000,
         },
         Image: {
             type: String,
