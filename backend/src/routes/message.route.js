ef168b5c1bc42c63dcde49953f07f92cc49b511d
Om Mahajna <omrmahajan9477@gmail.com>
2025-10-19 12:39:40 +0530

    added Message Route and sucessfully implemented everthing correctly

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,15 @@
 import express from "express";
+import { getAllContacts, getChatPartners, getMessagesByUserId, sendMessages } from "../controllers/message.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
 
-router.get("/send", (req, res) => {
-    res.send("Send message endpoint");
-});
+router.use(arcjetProtection ,protectRoute);
+
+router.get("/contacts" ,getAllContacts );
+router.get("/chats", getChatPartners );
+router.get("/:id" , getMessagesByUserId );
+router.post("/send/:id", sendMessages );
 
 export default router;
