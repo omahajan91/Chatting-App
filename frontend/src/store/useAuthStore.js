7539a8fa3955ec435905f2b70c36096e25a53d00
Om Mahajna <omrmahajan9477@gmail.com>
2025-10-27 19:19:46 +0530

    login Functionality

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -7,6 +7,7 @@ export const useAuthStore = create((set) => ({
   authUser: null,
   isCheckingAuth: true,
   isSigningUp: false,
+  isLoggingIn: false,
 
   checkAuth: async () => {
     try{
@@ -33,6 +34,22 @@ export const useAuthStore = create((set) => ({
       toast.error(error.response.data.message);
     }finally{
       set({isSigningUp:false})
+    }
+  },
+
+  login: async(data) => {
+    set({isLoggingIn:true})
+    try {
+      const res = await axiosInstance.post("/auth/login", data) ;
+      set({authUser: res.data});
+
+      // toast
+      toast.success("Logged in successfully");
+
+    }catch (error) {
+      toast.error(error.response.data.message);
+    }finally{
+      set({isLoggingIn:false})
     }
 
   }
