308be2de6384d76b96984e1c1b86f2e57c85c33b
Om Mahajna <omrmahajan9477@gmail.com>
2025-12-03 13:20:58 +0530

    image error resolved

--- backend/src/models/Message.js
+++ backend/src/models/Message.js
@@ -18,7 +18,7 @@ const messageSchema = new mongoose.Schema(
             trim: true,
             maxlength: 2000,
         },
-        Image: {
+        image: {
             type: String,
         },
     },   
